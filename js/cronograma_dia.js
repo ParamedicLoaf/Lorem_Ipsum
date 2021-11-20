0e6851fa5a3f8cd4934435593c5717f86ba2515c
Thiago Santos <tynamospark@gmail.com>
2021-11-20 12:37:30 -0300

    Menu lindo e maravilhoso

--- js/cronograma_dia.js
+++ js/cronograma_dia.js
@@ -34,5 +34,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
     })
 
+    aberto = false
+    menu = document.querySelector('.menu')
+    janela = document.querySelector('.janela')
+
+    menu.addEventListener('click', function(event) {
+        if (aberto) {
+            janela.style.height = 0
+            janela.style.opacity = 0
+            menu.innerHTML = '&#9776;'
+            aberto = false
+        }
+        else {
+            menu.innerHTML = '&#9887;'
+            janela.style.height = '30rem'
+            janela.style.opacity = '100%'
+            aberto = true
+        }
+    })
 
 })
